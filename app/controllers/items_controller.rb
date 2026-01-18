2416458d7b59d0fec00e0a08a2bfbd63ebf1679a
daisuke373-tokyo <daisuke373@gmail.com>
2026-01-18 10:11:03 +0900

    画面遷移の確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,10 +11,14 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to @item
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
